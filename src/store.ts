a07e44fa99aa88d431ddc7ba5fc74d3dc8f22c3c
Michael <zhaihaohk@gmail.com>
2020-05-14 13:52:18 +1200

    :star: integrate react native debbuger

--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,22 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { createEpicMiddleware } from 'redux-observable';
+import createDebugger from 'redux-flipper';
 import rootReducer from './reducers';
 import rootEpic from './epics';
 
 const epicMiddleware = createEpicMiddleware();
-const middleware = applyMiddleware(epicMiddleware);
-const store = createStore(rootReducer, middleware);
+// Config for flipper debugging
+const flipperReduxDebugger = createDebugger();
+
+// Config for redux DevTools integration
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const middleware = applyMiddleware(
+  epicMiddleware,
+  flipperReduxDebugger
+);
+
+const store = createStore(rootReducer, composeEnhancers(middleware));
 
 epicMiddleware.run(rootEpic);
 
